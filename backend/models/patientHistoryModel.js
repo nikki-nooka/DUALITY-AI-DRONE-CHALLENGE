fcd35ed6bf3d541b892a150c7117f8a81e05076c
SaiVeekshithKotha <ksv3306@gmail.com>
2025-03-13 17:19:57 +0530

    modifying the patient_history, medicine_prescribed

--- backend/models/patientHistoryModel.js
+++ backend/models/patientHistoryModel.js
@@ -5,7 +5,16 @@ const PatientHistorySchema = new mongoose.Schema({
     visits: [{
       doctor_id: { type: Number, required: true },
       timestamp: { type: String, required: true, match: /^\d{4}-\d{2}$/ },
-      medicines_prescribed: [{ medicine_id: String, quantity: Number }],
+      medicines_prescribed: [{ 
+        medicine_id: String, 
+        quantity: Number,
+        dosage_schedule: {
+          days: Number,
+          morning: Boolean,
+          afternoon: Boolean,
+          night: Boolean
+        }
+      }],
       medicines_given: [{ medicine_id: String, quantity: Number }],
     }],
   });
